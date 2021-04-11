adad18006b87f3f6a04c91031399cc0fa720aa14
MissJuliaRobot <ayushchatterjee2003@gmail.com>
2021-04-11 19:31:55 +0530

    fix

--- julia/modules/_spamdetector.py
+++ julia/modules/_spamdetector.py
@@ -45,10 +45,8 @@ async def spammers(event):
     msg = event.text
 
     for (ent, txt) in event.get_entities_text():
-        if isinstance(ent, types.MessageEntityBotCommand):
-            pass
-        else:
-            return
+        if not isinstance(ent, types.MessageEntityBotCommand):          
+           return
 
     client = MongoClient(MONGO_DB_URI)
     db = client["missjuliarobot"]
@@ -65,7 +63,6 @@ async def spammers(event):
             endtime = datetime.now()
             count = to_check["count"]
             lastmsg = to_check["lastmsg"]   
-            media = to_check["media"]
             expiry = endtime + timedelta(days=1)
             if (
                  count > 4
@@ -73,9 +70,7 @@ async def spammers(event):
                  and int(((endtime - starttime)).total_seconds()) <= 3                            
             ) or (
                  count > 4 and event.sender_id == idiot and event.text == lastmsg
-            ) or (
-                 count > 4 and event.sender_id == idiot and event.media == media
-            ):
+            ): 
               if senderr.username is None:
                  pow = leechers.find({})
                  for z in pow:
@@ -107,13 +102,12 @@ async def spammers(event):
                     "etime": endtime,
                     "count": count,
                     "lastmsg": lastmsg, 
-                    "media": media,
                 },
                 {"$set": {"count": count + 1, "etime": endtime}},
             )
             return
           
-    spammers.insert_one({"id": sender, "stime": datetime.now(), "etime": None, "count": 1, "lastmsg": msg, "media": event.media})    
+    spammers.insert_one({"id": sender, "stime": datetime.now(), "etime": None, "count": 1, "lastmsg": msg})    
 
 
 @tbot.on(events.NewMessage(pattern=None))
